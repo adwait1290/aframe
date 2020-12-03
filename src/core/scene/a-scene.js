97c09e42313bed4d757dd0e15f9e9bb574632de4
Adwait Athale <adwaitathale@gmail.com>
2020-12-03 02:06:01 -0800

    fix

--- src/core/scene/a-scene.js
+++ src/core/scene/a-scene.js
@@ -298,10 +298,6 @@ module.exports.AScene = registerElement('a-scene', {
           } else {
             console.log("AFRAME !this.hasWebXR");
             vrDisplay = utils.device.getVRDisplay();
-            const asyncRequestPresent = async () => {
-              vrDisplay.requestPresent();
-            }
-            asyncRequestPresent();
             console.dir(vrDisplay);
             console.log("AFRAME vrDisplay ".concat(vrDisplay).concat(" ").concat(Object.keys(vrDisplay)));
             console.log("AFRAME vrManager ".concat(vrManager).concat(" ").concat(Object.keys(vrManager)));
